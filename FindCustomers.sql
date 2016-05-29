59d086e06a11d280915cf677590d8411109decb0
sergemelnikov <serg1.1986@yandex.ru>
2016-05-29 15:29:02 +0000

    Add files via upload

---
+++ FindCustomers.sql
@@ -0,0 +1,12 @@
+﻿select distinct c.name from new_schema.customers as c 
+inner join new_schema.purchaises as p
+on c.customerid = p.customerid
+where
+p.productname = "молоко"
+and not exists
+(
+select * from new_schema.purchaises as p1
+where c.customerid = p1.customerid
+and
+p1.productname = "сметана"  and TIMESTAMPDIFF(month,purchaisedatetime,now())<=1
+);
